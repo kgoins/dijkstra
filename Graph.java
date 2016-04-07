179ff4da29505e7b1e1334dd730d66002e159765
Kgoins <kgoins@Box0.local>
2016-04-07 03:13:47 -0400

    added min dist cost

--- Graph.java
+++ Graph.java
@@ -4,16 +4,44 @@ import java.util.HashMap;
 class Graph {
     private ArrayList<Node> nodes;
     private Node source;
+
     private HashMap<Node, Integer> dist;
     private HashMap<Node, Node> prevHop;
 
     public Graph(Node source, ArrayList<Node> nodes) {
         this.source = source;
         this.nodes = nodes;
+        resetMaps();
+    }
 
+    public void setSource(Node source) {
+        this.source = source;
+        runDijkstra();
+    }
+
+    private void resetMaps() {
         for(Node node : nodes) {
             dist.put(node, Integer.MAX_VALUE);
             prevHop.put(node, null);
         }
+    }
+
+    public void runDijkstra() {
+        resetMaps();
+    }
+
+    private Node minDistEntry() {
+        int minCost = Integer.MAX_VALUE;
+        Node minNode = null;
+
+        for(Node node : nodes) {
+            int cost = dist.get(node);
+            if(cost < minCost) {
+                minCost = cost;
+                minNode = node;
+            }
+        }
+
+        return minNode;
     }
 }
