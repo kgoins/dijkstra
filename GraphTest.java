665a85e05778df01d75d6781832ce79bfdc3786f
Kgoins <kgoins@Box0.local>
2016-04-10 02:56:06 -0400

    fixed error in harderTest.graphfile, added getNodes to Graph

--- GraphTest.java
+++ GraphTest.java
@@ -1,10 +1,11 @@
-import java.util.HashSet;
-
 class GraphTest {
     public static void main(String[] args) {
        GraphBuilder builder = new GraphBuilder();
        Graph easyTest = builder.build("test.graphfile");
        Graph harderTest = builder.build("harderTest.graphfile");
+
+       easyTest.runDijkstra();
+       harderTest.runDijkstra();
 
        System.out.println(easyTest);
        System.out.println();
